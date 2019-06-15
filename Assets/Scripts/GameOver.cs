4367768edef6105470b02c8a1c3dcaae139e6b50
Ahmedman60 <mohamedx_x2015@yahoo.com>
2019-06-15 04:01:25 +0200

    Updates
    The Game uploading file

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public partial class GameOver : MonoBehaviour {
+
+
+    // Use this for initialization
+    public static bool dieinlevle2;
+	void  OnGUI(){
+
+        //MoveCharFeto d = new MoveCharFeto();
+        
+		//if retry button is pressed load scene 0 the game
+		GUI.contentColor = Color.red;
+        if (MoveCharFeto.CurrentLevel == 1)
+        {
+            dieinlevle2 = true;
+        }
+        else { dieinlevle2 = false; }
+        if (SceneManager.GetActiveScene()==SceneManager.GetSceneByBuildIndex(2))
+        {
+            Debug.Log(SceneManager.GetActiveScene().buildIndex.ToString());
+            Debug.Log(dieinlevle2.ToString());
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 70, 100, 40), "Retry?"))
+            {
+                if (dieinlevle2 == true)
+                {
+                    SceneManager.LoadScene(1);
+                }
+                else
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+            //and quit button
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 120, 100, 40), "Quit"))
+            {
+                Application.Quit();
+            }
+        }
+	}
+	
+}
